9a3329b65dcae8d27a583dfcba4b91a727e7fe79
ismailraissi <ismailraissi@icloud.com>
2020-05-12 19:05:56 +0200

    add game

--- src/test/test.java
+++ src/test/test.java
@@ -4,4 +4,7 @@ public class test {
     private String auto;
     private String fiets;
     private String motor;
+
+    Game game1 = p1.zoekGameOpNaam("Just Cause 3");
+System.out.println("p1 heeft Just Cause 3 " + (game1 != null ? "wel!" : "niet!"));
 }
